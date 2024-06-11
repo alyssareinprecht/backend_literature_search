207e24434f3cd4149be214d086222ecd15b27987
Chiara-Valentin <161332401+Chiara-Valentin@users.noreply.github.com>
2024-06-11 21:36:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def download_database():
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = sqlite3.connect(DATABASE__PATH)
     return db
 
 @app.teardown_appcontext
