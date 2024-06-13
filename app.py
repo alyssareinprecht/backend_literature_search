ae5b129a6072d3acf2725b84e99467ee5a3e6e94
Chiara-Valentin <161332401+Chiara-Valentin@users.noreply.github.com>
2024-06-13 16:32:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, g, jsonify, render_template
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from ast import literal_eval
 
-app = Flask (__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-##### Load Dataframe
-df = pd.read_csv('data/research_papers_with_wordinfo.csv')
+##### Load data in 
+df = pd.read_csv('data/papers.csv')
 df['keywords_scaled_importance'] = df['keywords_scaled_importance'].apply(literal_eval)
 df['word_frequency_dict'] = df['word_frequency_dict'].apply(literal_eval)
 
-##### Function to retrieve papers with keywords 
+##### Function to rank papers 
 def rank_papers(df, keyword_list):
     keyword_list = keyword_list[:4]
     
@@ -23,3 +23,19 @@ def rank_papers(df, keyword_list):
     df['score'] = df['keywords_scaled_importance'].apply(score_paper)
     sorted_df = df.sort_values(by='score', ascending=False, kind='mergesort')
     return sorted_df
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+##### Handle requests 
+@app.route('/rank_papers', methods=['POST'])
+def get_ranked_papers():
+    data = request.json
+    keywords = data.get('keywords', [])
+    ranked_df = rank_papers(df, keywords)
+    papers = ranked_df[['title', 'word_frequency_dict']].to_dict(orient='records')
+    return jsonify(papers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
