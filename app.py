613453466147f5f9e27fa6cb4fcc2bec024fdb26
Chiara-Valentin <161332401+Chiara-Valentin@users.noreply.github.com>
2024-06-13 11:10:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,92 +1,25 @@
 from flask import Flask, g, jsonify, render_template
-import sqlite3
-import os
-import requests
+import pandas as pd
+from ast import literal_eval
 
 app = Flask (__name__)
 
-DATABASE_URL = 'https://drive.google.com/uc?export=download&id=1vImNtUtb61c6n7QMPLqO6MzUrPX7vPyT'
-DATABASE_PATH = 'database.db'
+##### Load Dataframe
+df = pd.read_csv('data/research_papers_with_wordinfo.csv')
+df['keywords_scaled_importance'] = df['keywords_scaled_importance'].apply(literal_eval)
+df['word_frequency_dict'] = df['word_frequency_dict'].apply(literal_eval)
 
-def download_database():
-    if not os.path.exists(DATABASE_PATH):
-        print("Downloading database file...")
-        response = requests.get(DATABASE_URL, stream=True)
-        with open(DATABASE_PATH, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
-        print("Database downloaded successfully.")
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE__PATH)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-@app.route("/")
-def home ():
-    return "Hello World, from Flask!"
-@app.route("/tables")
-def get_all_tables():
-    db = get_db()
-    cursor = db.cursor()
-
-    # Get the names of all tables in the database
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-
-    all_data = {}
-    for table_name in tables:
-        table_name = table_name[0]  # Extract the table name from the tuple
-        cursor.execute(f"SELECT * FROM {table_name}")
-        rows = cursor.fetchall()
-
-        # Get column names
-        cursor.execute(f"PRAGMA table_info({table_name});")
-        columns = [column[1] for column in cursor.fetchall()]
-
-        # Prepare data for JSON
-        table_data = []
-        for row in rows:
-            row_data = {columns[i]: row[i] for i in range(len(columns))}
-            table_data.append(row_data)
-        
-        all_data[table_name] = table_data
-
-    return jsonify(all_data)
-
-@app.route("/show-tables")
-def show_tables():
-    db = get_db()
-    cursor = db.cursor()
-
-    # Get the names of all tables in the database
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-
-    all_data = {}
-    for table_name in tables:
-        table_name = table_name[0]  # Extract the table name from the tuple
-        cursor.execute(f"SELECT * FROM {table_name}")
-        rows = cursor.fetchall()
-
-        # Get column names
-        cursor.execute(f"PRAGMA table_info({table_name});")
-        columns = [column[1] for column in cursor.fetchall()]
-
-        # Prepare data for rendering in template
-        table_data = []
-        for row in rows:
-            row_data = {columns[i]: row[i] for i in range(len(columns))}
-            table_data.append(row_data)
-        
-        all_data[table_name] = table_data
-
-    return render_template('tables.html', tables=all_data)
+##### Function to retrieve papers with keywords 
+def rank_papers(df, keyword_list):
+    keyword_list = keyword_list[:4]
+    
+    def score_paper(keywords_importance):
+        keywords_dict = dict(keywords_importance)
+        score = []
+        for keyword in keyword_list:
+            score.append(keywords_dict.get(keyword, 0))
+        return tuple(score)
+    
+    df['score'] = df['keywords_scaled_importance'].apply(score_paper)
+    sorted_df = df.sort_values(by='score', ascending=False, kind='mergesort')
+    return sorted_df
