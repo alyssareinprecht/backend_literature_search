77cd585a7420f11d6c1f89b557f768faee18ff6a
Alyssa Reinprecht <alyssa.reinprecht1@gmail.com>
2024-06-21 20:42:34 +0200

    priority ranking functionality

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ df['keyword_scaled_importance'] = df['keyword_scaled_importance'].apply(literal_
 df['word_frequency_dict'] = df['word_frequency_dict'].apply(literal_eval)
 
 # Function to rank papers
-def rank_papers(df, include_keywords, exclude_keywords):
+def rank_papers(df, include_keywords):
     include_keywords = include_keywords[:5]  # Limiting to 5
 
     def score_paper(keywords_importance):
         keywords_dict = dict(keywords_importance)
-        score = sum(keywords_dict.get(keyword, 0) for keyword in include_keywords)
+        score = sum(keywords_dict.get(keyword['keyword'], 0) * keyword['priority'] for keyword in include_keywords)
         return score if score > 0 else None
 
     df['score'] = df['keyword_scaled_importance'].apply(score_paper)
@@ -34,13 +34,12 @@ def index():
 def get_ranked_papers():
     data = request.json
     include_keywords = data.get('include', [])
-    exclude_keywords = data.get('exclude', [])
 
-    # Check if both include and exclude keywords are empty
-    if not include_keywords and not exclude_keywords:
+    # Check if include keywords are empty
+    if not include_keywords:
         papers = df[['title', 'word_frequency_dict']].to_dict(orient='records')
     else:
-        ranked_df = rank_papers(df, include_keywords, exclude_keywords)
+        ranked_df = rank_papers(df, include_keywords)
         papers = ranked_df[['title', 'word_frequency_dict']].to_dict(orient='records')
     
     return jsonify(papers)
